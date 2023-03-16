b79155d45e0f48bc5ff6b64821685a19a377a45f
Hoang An <contact@unicode.vn>
2023-03-16 16:12:08 +0700

    Update code

--- react_hook/src/Components/Video/Video.js
+++ react_hook/src/Components/Video/Video.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
 import Media from "./Media";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 
 const Video = () => {
   console.log("re-render");
   const videoRef = useRef();
+  const btnRef = useRef();
 
   const [isPlay, setIsPlay] = useState(false);
 
@@ -19,16 +22,29 @@ const Video = () => {
   };
 
   useEffect(() => {
-    console.log(videoRef.current.duration);
+    //console.log(videoRef.current.duration);
+    console.log(btnRef);
+    btnRef.current.classList.remove("btn-primary");
+    btnRef.current.classList.add("btn-danger");
   }, []);
 
   return (
     <div>
       <Media ref={videoRef} />
-      <p>{videoRef.current.duration}</p>
+      {/* <p>{videoRef.current.duration}</p> */}
       <button type="button" onClick={handlePlay}>
         {isPlay ? "Pause" : "Play"}
       </button>
+      <Button
+        variant="primary"
+        ref={btnRef}
+        onClick={(e) => {
+          e.target.classList.add("abc");
+          //console.log(e.target.classList.add("abc"));
+        }}
+      >
+        Test Btn
+      </Button>
     </div>
   );
 };
